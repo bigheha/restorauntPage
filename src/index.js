1e6394a92df0714901735104647685831014256d
bigheha <chispinsky1337@gmail.com>
2023-01-09 20:17:33 +0200

    Fixed includement of css asset

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('alert');
+import landing from "./landing";
+import './styles.css';
+const content = document.querySelector('.content');
+content.appendChild(landing);
---
+++ src/landing.js
@@ -0,0 +1,15 @@
+const landing = document.createElement('div');
+
+const heading = document.createElement('h1');
+heading.innerHTML = 'Podolia restoraunt'
+landing.appendChild(heading);
+
+const image = document.createElement('img');
+image.setAttribute('src', 'hoverla.jpg');
+landing.appendChild(image);
+
+const text = document.createElement('p');
+text.innerHTML = 'Podolia is a restoraunt dedicated to the cuisine of Ukrainian region of Podolia, that enjoyed influences not only from Ukraine but from Poland, Hungary and even Turkey.';
+landing.appendChild(text);
+
+export default landing;
